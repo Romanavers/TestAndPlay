e16e3c867674b51041e23c21efcd92286b2867e4
Romanavers <roman.avershyn@gmail.com>
2015-11-23 22:14:01 -0500

    test_ProjectOneTest

---
+++ ProjectOneTest/ProjectOneTest/MyTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ProjectOneTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+ 
+namespace ProjectOneTest
+{
+    [TestClass]
+    public class MyTest1
+    {
+        IWebDriver driver;
+
+        [TestMethod]
+        public void VerifyTitle()
+        {
+            //write test
+            var title = driver.Title;
+            Assert.AreEqual("Products | Done The Deal", title);
+        }
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://donethedeal.com");
+            //start browser and open URL
+        }
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            //Close Browser
+            driver.Quit();
+        }
+    }
+
+}
