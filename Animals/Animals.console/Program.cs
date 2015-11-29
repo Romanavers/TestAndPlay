6942e299da46a8557cb7e79055884956e0d23aad
Romanavers <roman.avershyn@gmail.com>
2015-11-28 19:59:16 -0500

    Animals

---
+++ Animals/Animals.console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Archie");
+            Cat cat = new Cat("Tom");
+            dog.Eat();
+            Shit dogShit = dog.Poop();
+            dogShit.Clean();
+            cat.Eat();
+            Shit catShit = cat.Poop();
+            catShit.Clean();
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Animals/Animals/Class1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public interface IAnimal
+    {
+        void Eat();
+        Shit Poop();
+        void Enjoy();
+        string GetName();
+    }
+
+    public class Shit
+    {
+        public void Clean()
+        {
+            Console.WriteLine("Clean");
+        }
+
+    }
+
+    public class Dog : IAnimal
+    {
+        private string Name;
+        public Dog(string name)
+        {
+            Name = name;
+        }
+        public void Eat()
+        {
+            string eatString = string.Format("{0} ate", GetName());
+            Console.WriteLine(eatString);
+        }
+
+        public void Enjoy()
+        {
+            Console.WriteLine("{0} enjoyed", GetName());
+        }
+
+        public Shit Poop()
+        {
+            Shit shit = new Shit();
+            Console.WriteLine("Pooped");
+            return shit;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        private string Name;
+        public Cat(string name)
+        {
+            Name = name;
+        }
+        public void Eat()
+        {
+            string eatString = string.Format("{0} ate", GetName());
+            Console.WriteLine(eatString);
+        }
+
+        public void Enjoy()
+        {
+            Console.WriteLine("{0} enjoyed", GetName());
+        }
+
+        public Shit Poop()
+        {
+            FindPlace();
+            Shit shit = new Shit();
+            Console.WriteLine("Pooped");
+            return shit;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        private void FindPlace()
+        {
+            Console.WriteLine("Found place");
+        }
+    }
+
+
+
+}
+
+
